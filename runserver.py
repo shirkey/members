5f0d8f21df2d20d0f7672960e2a771021ff153c1
Isman Firmansyah <isman.firmansyah@gmail.com>
2014-07-04 21:22:54 +0700

    use url_for to render static files

--- runserver.py
+++ runserver.py
@@ -6,7 +6,6 @@ see http://flask.pocoo.org/docs/patterns/packages/#larger-applications
 import optparse
 
 from users import APP, LOGGER
-from users.static import static_file
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
@@ -19,8 +18,6 @@ if __name__ == '__main__':
         print 'Running in debug mode'
         LOGGER.info('Running in debug mode')
         APP.debug = True
-        # set up flask to serve static content
-        APP.add_url_rule('/<path:path>', 'static_file', static_file)
     else:
         print 'Running in production mode'
         LOGGER.info('Running in production mode')
