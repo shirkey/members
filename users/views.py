25d5a99d93eb0851864802132562844cd5c21ca9
Isman Firmansyah <isman.firmansyah@gmail.com>
2014-07-07 13:16:03 +0700

    revert url_for magic for generating links in email template

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@
 """
 import json
 
-from flask import render_template, Response, request, url_for
+from flask import render_template, Response, request
 from werkzeug.exceptions import default_exceptions
 
 # App declared directly in __init__ as per
@@ -153,9 +153,7 @@ def add_user_view():
     body = render_template(
         'text/registration_confirmation_email.txt',
         project_name=APP.config['PROJECT_NAME'],
-        url=url_for(".map_view", _external=True),
-        edit_url=url_for(".edit_user_view", guid=added_user["guid"],
-                         _external=True),
+        url=APP.config["PUBLIC_URL"],
         user=added_user)
     recipient = added_user['email']
     send_async_mail(
@@ -309,7 +307,7 @@ def delete_user_view(guid):
     """
     # Delete User
     delete_user(guid)
-    return url_for(".map_view")
+    return APP.config["PUBLIC_URL"]
 
 
 @APP.route('/download')
@@ -364,8 +362,7 @@ def reminder_view():
     body = render_template(
         'text/registration_confirmation_email.txt',
         project_name=APP.config['PROJECT_NAME'],
-        url=url_for(".map_view", _external=True),
-        edit_url=url_for(".edit_user_view", guid=user["guid"], _external=True),
+        url=APP.config["PUBLIC_URL"],
         user=user)
     send_async_mail(
         sender=MAIL_ADMIN,
